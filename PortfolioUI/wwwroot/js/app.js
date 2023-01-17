1fee0ba98dcdd0a736717f780f90b299180023ad
harrihonkanen <devharri@outlook.com>
2023-01-17 20:50:29 +0200

    Added javascript to close menu in mobile view. Fixed scrolling to about section

---
+++ PortfolioUI/wwwroot/js/app.js
@@ -0,0 +1,16 @@
+﻿
+document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("nav-link")) {
+
+        var navbarCollapse = document.getElementById('navbarNavAltMarkup');
+        if (navbarCollapse.classList.contains("show")) {
+            var bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+                toggle: true
+            });
+        }
+    }
+});
+
+function mymessage() {
+    alert("This message was triggered from the onload event");
+}
