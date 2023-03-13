2062166e655921d33cc24af72935f6b8e79d61dd
harrihonkanen <devharri@outlook.com>
2023-03-13 20:50:38 +0200

    Changed contact form to blazor component, added appsettings.json, added GoogleReCaptcha to contact form

---
+++ PortfolioUI/Models/ContactFormModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PortfolioUI.Models;
+
+public class ContactFormModel
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Message { get; set; }
+    public string? Token { get; set; }
+
+
+}
+
