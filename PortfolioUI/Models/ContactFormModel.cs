88a2843365add5493564e48f9d2b9151596a0bae
harrihonkanen <devharri@outlook.com>
2023-03-14 19:10:21 +0200

    Started a validation for contact form

--- PortfolioUI/Models/ContactFormModel.cs
+++ PortfolioUI/Models/ContactFormModel.cs
@@ -1,9 +1,14 @@
 ﻿namespace PortfolioUI.Models;
+using System.ComponentModel.DataAnnotations;
 
 public class ContactFormModel
 {
+    [Required]
     public string? FirstName { get; set; }
+    [Required]
     public string? LastName { get; set; }
+    [Required]
     public string? Message { get; set; }
+    [Required]
     public string? Token { get; set; }
 }
