825bd6ee55ae5d750bb2b42a490217e2c643d9c4
harrihonkanen <devharri@outlook.com>
2023-03-18 10:42:11 +0200

    Added comments, added captcha service,

--- PortfolioUI/Program.cs
+++ PortfolioUI/Program.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PortfolioUI;
+using PortfolioUI.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddTransient<GoogleCaptchaService>();
 
 await builder.Build().RunAsync();
 
---
+++ PortfolioUI/Services/GoogleCaptchaService.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace PortfolioUI.Services;
+
+public class GoogleCaptchaService
+{
+    //Post request to Web API with token in query
+    public async Task<bool> VerifyToken(string token)
+    {
+        try
+        {
+            using (var client = new HttpClient())
+            {
+                var url = $"https://localhost:7295/api/recaptcha?token={token}";
+
+                var httpResult = await client.GetAsync(url);
+                if (httpResult.StatusCode != HttpStatusCode.OK)
+                {
+                    return false;
+                }
+
+                var responseString = await httpResult.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<bool>(responseString);
+
+                return result;
+            }
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+}
